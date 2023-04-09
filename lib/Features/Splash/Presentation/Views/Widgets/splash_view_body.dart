857abdb341b6aeb7ce11c65712319c47f122637e
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-04-09 11:09:47 +0200

    init the application added splash screen with animated text

---
+++ lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
@@ -0,0 +1,48 @@
+import 'package:bookly/Features/Splash/Presentation/Views/Widgets/sliding_text.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../Core/Utils/AppAssets.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 20), end:Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          AppAssets.kLogo,
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        SlidingTextWidget(slidingAnimation: slidingAnimation,),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bookly/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'Features/Splash/Presentation/Views/SplashView.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: const SplashView(),
+    );
+  }
+}
