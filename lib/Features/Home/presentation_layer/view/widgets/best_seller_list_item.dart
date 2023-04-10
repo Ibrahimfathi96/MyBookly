3b09fdd3dba2919384ce5762797f0635cfbd0683
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-04-10 16:49:12 +0200

    bestSeller Item done

--- lib/Features/Home/presentation_layer/view/widgets/best_seller_list_item.dart
+++ lib/Features/Home/presentation_layer/view/widgets/best_seller_list_item.dart
@@ -1,5 +1,7 @@
 import 'package:bookly/Core/Utils/styles.dart';
+import 'package:bookly/Features/Home/presentation_layer/view/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../Core/Utils/AppAssets.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -12,47 +14,54 @@ class BestSellerListViewItem extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          AspectRatio(
-            aspectRatio: 2.5/4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.white,
-                  image: const DecorationImage(
-                      image: AssetImage(AppAssets.kBook),
-                      fit: BoxFit.cover,
-                  ),
+          Padding(
+            padding: const EdgeInsets.only(right: 30.0),
+            child: AspectRatio(
+              aspectRatio: 2.5/4,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.white,
+                    image: const DecorationImage(
+                        image: AssetImage(AppAssets.kBook),
+                        fit: BoxFit.cover,
+                    ),
+                ),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width /2,
-                  child: const Text(
-                    'Harry Potter and the Goblet of Fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle20N,
-                  ),
-                ),
-                const SizedBox(height: 3,),
-                const Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle14,
-                ),
-                const SizedBox(height: 3,),
-                Row(
-                  children: const [
-                    Text('19.99 €',
-                    style: Styles.textStyle20,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width /2,
+                    child: const Text(
+                      'Harry Potter and the Goblet of Fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle20N,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 3,),
+                  const Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  const SizedBox(height: 3,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text('19.99 €',
+                      style: Styles.textStyle20,
+                      ),
+                      BooksRating(),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
@@ -60,3 +69,4 @@ class BestSellerListViewItem extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/Features/Home/presentation_layer/view/widgets/book_rating.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/Core/Utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BooksRating extends StatelessWidget {
+  const BooksRating({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Icon(FontAwesomeIcons.solidStar,color: Color(0xffFFDD4F),size: 18,),
+        SizedBox(width: 7,),
+        Text('4.8',style:Styles.textStyle16 ,),
+        SizedBox(width: 9,),
+        Text('(2930)',style:Styles.textStyle14 ,),
+      ],
+    );
+  }
+}
