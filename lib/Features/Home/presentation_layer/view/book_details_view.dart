371ee4d93fdb8b013c26269fa6fbe94929061978
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-04-11 17:17:41 +0200

    create book details view

---
+++ lib/Features/Home/presentation_layer/view/book_details_view.dart
@@ -0,0 +1,34 @@
+import 'package:bookly/Features/Home/presentation_layer/view/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../Constants.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: kPrimaryColor,
+        automaticallyImplyLeading: false,
+        title: BookDetailsAppBar(),
+      ),
+      body: BookDetailsViewBody(),
+    );
+  }
+}
+
+class BookDetailsAppBar extends StatelessWidget {
+  const BookDetailsAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return Row(
+     children: [
+
+     ],
+   );
+  }
+}
