a4dd8934b210adfffccaf5c17eb55b71b6454582
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-04-14 00:09:45 +0200

    create home repo methods and failure file

--- lib/Features/Home/data_layer/repository/home_repo.dart
+++ lib/Features/Home/data_layer/repository/home_repo.dart
@@ -1,4 +1,12 @@
+import 'package:bookly/Core/errors/failure.dart';
+import 'package:bookly/Features/Home/data_layer/models/book_model/BookModel.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo{
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  /*Either class allows us to return more than one type
+  * for example here we want List of book model as a return type
+  * but what if the the api call failed? we need another return type to handle this failure here
+  * so Either is a good way to handle the error return type*/
+Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
